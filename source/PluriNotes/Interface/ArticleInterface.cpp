9ef70f151f858db24f4d98cec6504feeda7f62b3
rtva123 <tdavid2@live.fr>
2017-06-05 16:01:04 +0200

    Update ArticleInterface.cpp
    première version

--- source/PluriNotes/Interface/ArticleInterface.cpp
+++ source/PluriNotes/Interface/ArticleInterface.cpp
@@ -1,2 +1,14 @@
-#include "ArticleInterface.h"
+#include "ArticleInterfaceEditable.h"
 
+articleInterfaceEditable::saveArticle(){
+  article.texte = texte.text;
+  article.titre = titre.text;
+  article.actualiserDateModif(); //à définir
+}
+
+articleInterfaceEditable::articleInterfaceEditable(article& a){
+  article = a;
+  titre = new QLineEdit(article.titre,this);
+  texte = new QLineEdit(article.texte,this);
+  //disposition à revoir
+}
