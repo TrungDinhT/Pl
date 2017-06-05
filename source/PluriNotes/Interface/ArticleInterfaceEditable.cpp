335017f97591e4ac7fc45e717536475749be9ef5
rtva123 <tdavid2@live.fr>
2017-06-05 16:41:27 +0200

    Update ArticleInterfaceEditable.cpp

--- source/PluriNotes/Interface/ArticleInterfaceEditable.cpp
+++ source/PluriNotes/Interface/ArticleInterfaceEditable.cpp
@@ -10,5 +10,7 @@ articleInterfaceEditable::articleInterfaceEditable(article& a){
   article = a;
   titre = new QLineEdit(article.titre,this);
   texte = new QLineEdit(article.texte,this);
+  save = new QPushButton(/*objet QIcon,*/"sauvegarder",this);
+  connect(save,SIGNAL(clicked()),this,SLOT(saveArticle()));
   //disposition à revoir
 }
