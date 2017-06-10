0ce65761bb033bca50ef31da3afe427d07b30246
rtva123 <tdavid2@live.fr>
2017-06-11 00:06:31 +0200

    Create ArticleInterfaceEditable.cpp
    article => pointeur

--- source/PluriNotes/Interface/ArticleInterfaceEditable.cpp
+++ source/PluriNotes/Interface/ArticleInterfaceEditable.cpp
@@ -1,14 +1,14 @@
 #include "ArticleInterfaceEditable.h"
 
-articleInterfaceEditable::saveArticle(){
-  article.texte = texte.text;
-  //article.titre = titre.text;
-  article.actualiserDateModif(); //à définir
-  articleInterface a = new articleInterface(article);
-  delete this;
-}
+//articleInterfaceEditable::saveArticle(){
+//  article.texte = texte.text;
+//  //article.titre = titre.text;
+//  article.actualiserDateModif(); //à définir
+//  articleInterface a = new articleInterface(article);
+//  delete this;
+//}
 
-articleInterfaceEditable::articleInterfaceEditable(article& a){
+articleInterfaceEditable::articleInterfaceEditable(Article* a){
   article = a;
   //titre = new QLineEdit(article.titre,this);
   texte = new QLineEdit(article.texte,this);
