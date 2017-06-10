7819650ff09305d4e8bddbb13aab4bcd9acf3648
rtva123 <tdavid2@live.fr>
2017-06-10 18:29:37 +0200

    Create ArticleInterface.cpp

--- source/PluriNotes/Interface/ArticleInterface.cpp
+++ source/PluriNotes/Interface/ArticleInterface.cpp
@@ -1,16 +1,19 @@
 #include "ArticleInterface.h"
 
-void articleInterface::modifArticle(){
-  articleInterfaceEditable a = new articleInterfaceEditable(article);
-  delete this;
-}
+//void articleInterface::modifArticle(){
+//  articleInterfaceEditable a = new articleInterfaceEditable(article);
+//  delete this;
+//}
 
+//articleInterface::articleInterface(){
+//  delete this;
+//}
 articleInterface::articleInterface(article& a){
   article = a;
   titre = new QLineEdit(article.titre);
   texte = new QLineEdit(article.texte);
   modifier = new QPushButton(/*objet QIcon,*/"modifier");
-  connect(modifier,SIGNAL(clicked()),this,SLOT(modifArticle()));
+  //connect(modifier,SIGNAL(clicked()),this,SLOT(modifArticle()));
   //disposition à revoir
 
   principale = new QVBoxLayout(this);
