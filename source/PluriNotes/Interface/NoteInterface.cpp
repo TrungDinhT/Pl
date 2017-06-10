241bcd4e69d07d69e30cb541665ddeff11ff3859
rtva123 <tdavid2@live.fr>
2017-06-11 01:04:37 +0200

    Create NoteInterface.cpp

--- source/PluriNotes/Interface/NoteInterface.cpp
+++ source/PluriNotes/Interface/NoteInterface.cpp
@@ -1,8 +1,9 @@
 NoteInterface::NoteInterface(Note n){
-titre = new QLabel(n.titre,this);
-modifier = new QPushbutton("modifier",this);
-supprimer = new QPushbutton("supprimer",this);
-relier = new QPushbutton("relier",this);
+  titre = new QLabel(n->titre);
+//titre = new QLabel(n->titre,this);
+  modifier = new QPushbutton("modifier");
+  supprimer = new QPushbutton("supprimer");
+  relier = new QPushbutton("relier");
 //connect(modifier,SIGNAL(clicked()),this,SLOT(modifArticle()));
 
 }
