81d64f908a104618379e9694d3584a381aa2099c
rtva123 <tdavid2@live.fr>
2017-06-11 16:22:06 +0200

    Create GlobalInterface.cpp
    changement de version

--- source/PluriNotes/Interface/GlobalInterface.cpp
+++ source/PluriNotes/Interface/GlobalInterface.cpp
@@ -1,36 +1,46 @@
 GlobalInterface::GlobalIterface(){
   NM = getManager();
-  iterator<Note, NotesManager> it = NM.beginIt();
+  iterator<Note, NotesManager> itn = NM.beginIt();
+  Version::iterator itv = itn.getIterator();
   principale = new QGridLayout(this);
-  NI = new NoteInterface(it.current());
+  NI = new NoteInterface(&(itv.current()));
   principale.addWidget(NI,0,1);
   //RI = new RelationInterface() ;
   //MI = ManagerInterface();
   //TB  =MyQToolbar();
   connect(NI->modifier,SIGNAL(clicked()),this,SLOT(modifierNote()));
   connect(NI->relier,SIGNAL(clicked()),this,SLOT(miseEnRelationNote()));
-  connect(NI->supprimer,SIGNAL(clicked()),this,SLOT(sypprimerNote()));
+  connect(NI->supprimer,SIGNAL(clicked()),this,SLOT(supprimerNote()));
+  connect(NI->changerversion,SIGNAL(clicked()),this,SLOT(changerNersionNote()));
 
 
 }
 
-GlobalInterface::modifierNote(Note n){
+void GlobalInterface::modifierNote(Note n){
   delete NI;
   NIE = new NoteInterfaceEditable(n);
   principale.addWidget(NIE,0,1);
   connect(NI->save,SIGNAL(clicked()),this,SLOT(sauverNote()));
 }
-GlobalInterface::sauverNote(Note n){
+void GlobalInterface::sauverNote(Note n){
+  article->texte = texte.text;
+  article.actualiserDateModif(); //à définir
+  articleInterface a = new articleInterface(article);
+
   delete NIE;
   NI = new NoteInterface(n);
   principale.addWidget(NI,0,1);
   connect(NI->modifier,SIGNAL(clicked()),this,SLOT(modifierNote()));
   connect(NI->relier,SIGNAL(clicked()),this,SLOT(miseEnRelationNote()));
-  connect(NI->supprimer,SIGNAL(clicked()),this,SLOT(sypprimerNote()));
+  connect(NI->supprimer,SIGNAL(clicked()),this,SLOT(supprimerNote()));
 }
-GlobalInterface::supprimerNote(){
+void GlobalInterface::supprimerNote(){
 //à completer
 }
-GlobalInterface::miseEnRelationNote(){
+void GlobalInterface::miseEnRelationNote(){
+//à completer
+}
+
+void GlobalInterface::changerVersionNote(){
 //à completer
 }
