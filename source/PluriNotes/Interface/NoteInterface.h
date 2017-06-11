c2e915c65ce7a6b0416decb0601cbffae4b2311b
trungdinh <dinhthanhtrung1996@gmail.com>
2017-06-11 20:11:42 +0200

    modify save methods

---
+++ source/PluriNotes/Interface/NoteInterface.h
@@ -0,0 +1,30 @@
+#ifndef NOTEINERFACE_H
+#define NOTEINERFACE_H
+
+#include <QtWidgets> //inclu tout les QWidget
+#include "NoteManager/NoteManager.h"
+#include "NoteManager/notes.h"
+
+
+class NoteInterface : public QWidget {
+  Q_OBJECT
+  Version* version;
+  QLabel* titre;
+  QPushButton* modifier;
+  QPushButton* supprimer;
+  QPushButton* relier;
+  
+  
+public :
+  NoteInterface(Version* v);
+  
+public slots : 
+  //void modification();
+  //void suppression();
+  //void relation();
+
+
+};
+
+
+#endif // NOTEINERFACE_H
