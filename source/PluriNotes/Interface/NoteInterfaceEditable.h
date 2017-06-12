5a7731f693b94546994c807c1c5d3c61699d63f0
rtva123 <tdavid2@live.fr>
2017-06-12 18:21:29 +0200

    Update NoteInterfaceEditable.h

--- source/PluriNotes/Interface/NoteInterfaceEditable.h
+++ source/PluriNotes/Interface/NoteInterfaceEditable.h
@@ -4,21 +4,30 @@
 #include <QtWidgets>
 #include "NoteManager/NoteManager.h"
 #include "NoteManager/notes.h"
-
+//#include "globalInterface.h"
+class GLobalInterface;
 
 class NoteInterfaceEditable : public QWidget {
-  Q_OBJECT
+    Q_OBJECT
+    friend class GlobalInterface;
+protected:
+
+
   Version* version;
   QLineEdit* titre;
   QPushButton* save;
-
+  QPushButton* supprimer;
+  QPushButton* relier;
+  QPushButton* changerversion;
+  QPushButton* rendreversionactive;
   
 public :
   NoteInterfaceEditable(Version* v);
   
 public slots : 
+    virtual void saveNote() = 0;
 
+};
 
-}
 
 #endif //NOTEINERFACEEDITABLE_H
