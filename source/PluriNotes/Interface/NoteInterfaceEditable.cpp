8625c7a03d1f18b422a35ad69c75eff674ab8fdc
rtva123 <tdavid2@live.fr>
2017-06-12 18:20:56 +0200

    Update NoteInterfaceEditable.cpp

--- source/PluriNotes/Interface/NoteInterfaceEditable.cpp
+++ source/PluriNotes/Interface/NoteInterfaceEditable.cpp
@@ -1,8 +1,13 @@
 
-#include "Interface/NoteInterfaceEditable.h"
-NoteInterfaceEditable::NoteInterfaceEditable(Version* v){
-  version = v;
-  titre = new QLineEdit(v->titre);
-  sauver = new QPushButton("sauver");
+#include "NoteInterfaceEditable.h"
+NoteInterfaceEditable::NoteInterfaceEditable(Version* v):version(v),QWidget(){
+  //version = v;
+  titre = new QLineEdit(v->getTitre());
+  //sauver = new QPushButton("sauver");
   save = new QPushButton("save");
+  connect(save,SIGNAL(clicked()),this,SLOT(saveArticle()));
+  supprimer = new QPushButton("supprimer");
+  relier = new QPushButton("relier");
+  changerversion = new QPushButton("changer de version");
+  rendreversionactive = new QPushButton("rendre la version active");
 }
