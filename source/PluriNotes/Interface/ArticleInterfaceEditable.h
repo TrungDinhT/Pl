e4b4406509d0939a1b0b1b70fd82f318ffb75ba9
rtva123 <tdavid2@live.fr>
2017-06-14 06:46:25 +0200

    Create ArticleInterfaceEditable.h
    ajout de managerInterface

--- source/PluriNotes/Interface/ArticleInterfaceEditable.h
+++ source/PluriNotes/Interface/ArticleInterfaceEditable.h
@@ -4,20 +4,26 @@
 #include <QtWidgets>
 #include "NoteManager/NoteManager.h"
 #include "NoteManager/notes.h"
+#include "NoteInterfaceEditable.h"
+
+class GLobalInterface;
 
 class articleInterfaceEditable: public NoteInterfaceEditable{
-
     Q_OBJECT
-
+    friend class GlobalInterface;
 public:
    articleInterfaceEditable(Article* a = 0);
     
 public slots:
-   //void saveArticle();
+   virtual void saveNote();
 
+
+signals:
+    //void sauvegarde(Article* a);
 private:
    //QLineEdit* titre;
    QTextEdit* text;
+   QVBoxLayout* principale;
    //QPushButton* save;
 
    Article* article;//Article* article;
