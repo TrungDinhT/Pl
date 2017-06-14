adfbdcf6ff1897149c5cc430d3d1ef949684afc8
rtva123 <tdavid2@live.fr>
2017-06-14 06:48:04 +0200

    Update globalInterface.h
    ajout managerInterface

--- source/PluriNotes/Interface/globalInterface.h
+++ source/PluriNotes/Interface/globalInterface.h
@@ -4,37 +4,37 @@
 #include <QtWidgets>
 #include "NoteManager/NoteManager.h"
 #include "NoteManager/notes.h"
-#include "Interface/NoteInterface.h"
-#include "Interface/NoteInterfaceEditable.h"
+#include "NoteInterfaceEditable.h"
+#include "managerinterface.h"
 
 
 
 class GlobalInterface : public QWidget {
   Q_OBJECT
   QGridLayout* principale;
-  NoteInterface* NI;
+  //NoteInterface* NI;
   NoteInterfaceEditable* NIE;
   //RelationInterface* RI;
-  //ManagerInterface* MI;
+  ManagerInterface* MI;
   //MyQToolbar* TB;
-  NoteManager& NM;
+  NotesManager* NM;
+  Note* NoteCurrent;
+  Version* VersionCurrent;
+  QListWidget* liste;
   
 public :
-  GlobalIterface();
+  GlobalInterface();
   
 public slots : 
-  void modifierNote();
-  void sauverNote();
+  //void modifierNote();
+  void changerNote(QListWidgetItem* item);
+  void sauverNote(Version* v);
   void supprimerNote();
   void miseEnRelationNote();
   void changerVersionNote();
   void versionActiveNote();
-  }
-
-
-
-
-
+  void choixVersionNote(QListWidgetItem* item);
+  };
 
 
 #endif // GLOBALINTERFACE_H
