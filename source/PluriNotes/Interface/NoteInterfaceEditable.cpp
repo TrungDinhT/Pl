e7da1b3a63226c29838cb7d5952f197e1137d47a
rtva123 <tdavid2@live.fr>
2017-06-14 06:53:13 +0200

    Update NoteInterfaceEditable.cpp
    ajout ManagerInterface

--- source/PluriNotes/Interface/NoteInterfaceEditable.cpp
+++ source/PluriNotes/Interface/NoteInterfaceEditable.cpp
@@ -1,8 +1,13 @@
 
-#include "Interface/NoteInterfaceEditable.h"
-NoteInterfaceEditable::NoteInterfaceEditable(Version* v){
-  version = v;
-  titre = new QLineEdit(v->titre);
-  sauver = new QPushbutton("sauver");
-
+#include "NoteInterfaceEditable.h"
+NoteInterfaceEditable::NoteInterfaceEditable(Version* v):version(v),QWidget(){
+  //version = v;
+  titre = new QLineEdit(v->getTitre());
+  //sauver = new QPushButton("sauver");
+  save = new QPushButton("save");
+  connect(save,SIGNAL(clicked()),this,SLOT(saveNote()));
+  supprimer = new QPushButton("supprimer");
+  relier = new QPushButton("relier");
+  changerversion = new QPushButton("changer de version");
+  rendreversionactive = new QPushButton("rendre la version active");
 }
